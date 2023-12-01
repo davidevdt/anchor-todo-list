ed15b80423f75ee6523dd43843b1874a815bcc07
davidevdt <davide.sbenson@gmail.com>
2023-12-01 18:59:43 +0100

    Updated todo-list program

--- programs/todo-list-app/src/lib.rs
+++ programs/todo-list-app/src/lib.rs
@@ -1,15 +1,21 @@
 use anchor_lang::prelude::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("5DKzJ39FkCy4B6Y9NBhPVDAmd8BrJdR8RBTbCMYFSUH9");
 
 #[program]
 pub mod todo_list_app {
     use super::*;
 
+    // The Context contains essential information about the program’s execution context, including accounts, program IDs, and more. 
+    // In this case, it holds references to the accounts involved in the transaction, such as the task being added and 
+    // the author’s account.
+    // The text parameter is a string representing the content of the new task that the user wants to add
+    // Ownership and the ability to modify the state of an account are crucial concepts when working with Solana accounts. Here, the program has ownership of the “Task” accounts it creates. 
+    // This means that the program has the authority to modify the data stored in those accounts.
     pub fn adding_task(ctx: Context<AddingTask>, text: String) -> Result<()> {
-        let task = &mut ctx.accounts.task;
-        let author = &ctx.accounts.author; // The `author` account
-        let clock = Clock::get().unwrap(); // Getting the current timestamp
+        let task = &mut ctx.accounts.task; // creates a mutable reference to the task account. As accounts are immutable by default, we need to modify the fields of the task to add new information like the task text, author, timestamps, etc
+        let author = &ctx.accounts.author; // The `author` account - This account represents the person who is adding the new task to the to-do list
+        let clock = Clock::get().unwrap(); // Getting the current timestamp -  fetches the current timestamp from the blockchain using the Clock service provided by Solana
         
         if text.chars().count() > 400 {
             return Err(ErrorCode::TextTooLong.into());
@@ -21,85 +27,78 @@ pub mod todo_list_app {
         task.updated_at = clock.unix_timestamp;
         task.text = text;
         Ok(())
-
     }
 
+    // This function is used to update the status of a task (whether it’s done or not).
     pub fn updating_task(ctx: Context<UpdatingTask>, is_done: bool) -> Result<()> {
         let task = &mut ctx.accounts.task;
         let author = &ctx.accounts.author; // The `author` account
         let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
         task.author = *author.key;
         task.is_done = is_done;
         task.updated_at = clock.unix_timestamp;
         Ok(())
-       
     }
 
+    // This function is used to mark a task as done, effectively “deleting” it from the active tasks list
     pub fn deleting_task(ctx: Context<DeletingTask>) -> Result<()> {
         let task = &mut ctx.accounts.task;
         let author = &ctx.accounts.author; // The `author` account
         let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
         task.author = *author.key;
         task.is_done = true;
         task.updated_at = clock.unix_timestamp;
         Ok(())
-       
     }
 
-
-
-
 }
 
 #[derive(Accounts)]
-pub struct AddingTask<'info> {
-    #[account(init, payer = author, space = Task::LEN)]
-    pub task: Account<'info, Task>,
-    #[account(mut)]
-    pub author: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub struct AddingTask<'info> { // The lifetime parameter is used to ensure that the accounts referenced in the struct are valid for the duration of the instruction execution.
+    #[account(init, payer = author, space = Task::LEN)] // we declare that we are initialising, author is paying for it and the space would be LEN as we defined.
+    pub task: Account<'info, Task>, // It represents an account of the type Task and will be used to read or modify data stored in the Task account during the instruction execution.
+    #[account(mut)] // the author parameter should be mutable as we will be changing the balance of the account of the author when he pays
+    pub author: Signer<'info>, // It represents a signer for the transaction, indicating that the author needs to sign the transaction for this instruction to be executed.
+    pub system_program: Program<'info, System>, //  This is the official System program from Solana. As programs are stateless, we even need to pass through the official System Program. This program will be used to initialise the Task account.
 }
-
-
-#[derive(Accounts)]
-pub struct UpdatingTask<'info> {
-    #[account(mut, has_one = author)]
-    pub task: Account<'info, Task>,
-    pub author: Signer<'info>,
-}
-
-#[derive(Accounts)]
-pub struct DeletingTask<'info> {
-    #[account(mut, has_one = author)]
-    pub task: Account<'info, Task>,
-    pub author: Signer<'info>,
-}
-
 
 #[account]
 pub struct Task {
-    pub author: Pubkey,  // The account that owns the task
-    pub is_done: bool,   // Whether the task is done or not
-    pub text: String,    // The text of the task
+    pub author: Pubkey, // The account that owns the task
+    pub is_done: bool, // Whether the task is done or not
+    pub text: String, // The text of the task
     pub created_at: i64, // The timestamp when the task was created
     pub updated_at: i64, // The timestamp when the task was last updated
 }
 
-const DISCRIMINATOR: usize = 8;
+#[derive(Accounts)]
+    pub struct UpdatingTask<'info> {
+    #[account(mut, has_one = author)]
+    pub task: Account<'info, Task>,
+    pub author: Signer<'info>,
+}
+
+#[derive(Accounts)]
+    pub struct DeletingTask<'info> {
+    #[account(mut, has_one = author)]
+    pub task: Account<'info, Task>,
+    pub author: Signer<'info>,
+}
+
+const DISCRIMINATOR: usize = 8; // used to distinguish (through SHA-256 hash of the name, 'Task') from other structs
 const PUBLIC_KEY_LENGTH: usize = 32;
 const BOOL_LENGTH: usize = 1;
-const TEXT_LENGTH: usize = 4 + 400 * 4; // 400 chars
+const TEXT_LENGTH: usize = 4 + 400 * 4; // 400 chars (max)
 const TIMESTAMP_LENGTH: usize = 8;
 
 impl Task {
+    // it can be accessed as Task::LEN
     const LEN: usize = DISCRIMINATOR + // discriminator
-        PUBLIC_KEY_LENGTH + // author
-        BOOL_LENGTH + // is_done
-        TEXT_LENGTH +  // text
-        TIMESTAMP_LENGTH + // created_at
-        TIMESTAMP_LENGTH; // updated_at
+    PUBLIC_KEY_LENGTH + // author
+    BOOL_LENGTH + // is_done
+    TEXT_LENGTH + // text
+    TIMESTAMP_LENGTH + // created_at
+    TIMESTAMP_LENGTH; // updated_at
 }
 
 
--- tests/todo-list-app.ts
+++ tests/todo-list-app.ts
@@ -3,34 +3,37 @@ import { Program } from "@coral-xyz/anchor";
 import { TodoListApp } from "../target/types/todo_list_app";
 import { assert } from "chai";
 
-describe("todo-list-app", () => {
-  // Configure the client to use the local cluster.
+
+describe("todo-list-app", () => {   // test-suite name 
+  // Configure the client to use the local Solana cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-  const program = anchor.workspace.TodoListApp as Program<TodoListApp>;
-  const author = program.provider as anchor.AnchorProvider;
-  it("can create a task", async () => {
-    const task = anchor.web3.Keypair.generate();
-    const tx = await program.methods
-      .addingTask("You are awesome")
-      .accounts({
-        task: task.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .signers([task])
-      .rpc();
-    console.log("Your transaction signature", tx);
-
-    const taskAccount = await program.account.task.fetch(task.publicKey);
-    console.log("Your task", taskAccount);
-
-    assert.equal(
-      taskAccount.author.toBase58(),
-      author.wallet.publicKey.toBase58()
-    );
-    assert.equal(taskAccount.text, "You are awesome");
-    assert.equal(taskAccount.isDone, false);
-    assert.ok(taskAccount.createdAt);
-    assert.ok(taskAccount.updatedAt);
+  const program = anchor.workspace.TodoListApp as Program<TodoListApp>; //  initialises the program variable with the Solana program object defined in the to-doListApp workspace
+  const author = program.provider as anchor.AnchorProvider; // initialises the author variable with the program’s provider
+  it("can create a task", async () => { //  starts a new test case within the test suite with the description “can create a task.”
+  const task = anchor.web3.Keypair.generate(); // The private key of this keypair will sign the transaction so that the program can take up the ownership and use the public key of this keypair for the task account
+  const tx = await program.methods 
+  .addingTask("You are awesome") // This line calls the addingTask method of the program, which represents the adding_task function we previously discussed in the blog. It adds a new task with the text “You are awesome.” The method is asynchronous (async) as it interacts with the blockchain. The method returns a transaction (tx) that will be signed and sent to the network later.
+  .accounts({ // specifies the accounts involved in the transaction. In this case, the task account is used as an argument to the adding_task function.
+  task: task.publicKey, 
+  systemProgram: anchor.web3.SystemProgram.programId,
+  })
+  .signers([task]) // we will add the task keypair here. If you are using a keypair other than the provider as the author, that keypair will also be required in the signers’ array.
+  .rpc(); //  sends the transaction to the Solana network (RPC) and waits for confirmation. 
+  console.log("Your transaction signature", tx); 
+  
+  
+  const taskAccount = await program.account.task.fetch(task.publicKey); // This line fetches the task account created during the transaction by using the task’s public key (task.publicKey). It retrieves the most recent state of the task account from the blockchain.
+  console.log("Your task", taskAccount); // This line logs the contents of the fetched task account to the console. It allows us to inspect the task account after it has been created and updated.
+  
+  
+  assert.equal( // checks if the task’s author matches the author’s wallet public key. Since the author variable holds the program’s provider, author.wallet.publicKey gives the public key of the author’s wallet.
+  taskAccount.author.toBase58(),
+  author.wallet.publicKey.toBase58()
+  );
+  assert.equal(taskAccount.text, "You are awesome"); // This line checks if the task’s text matches the text provided during the task creation.
+  assert.equal(taskAccount.isDone, false);
+  assert.ok(taskAccount.createdAt);
+  assert.ok(taskAccount.updatedAt);
   });
 
   // it("can create a new task from a different author", async () => {
@@ -136,4 +139,5 @@ describe("todo-list-app", () => {
   //     assert.equal(err.toString(), "Error: failed to send transaction");
   //   }
   // });
-});
+
+  });
